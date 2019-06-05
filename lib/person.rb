21167fe5e9c46d5d6a899b9fc75376a82e00f61c
Alexander Schelchere <ascellie@gmail.com>
2019-06-05 11:51:50 -0400

    initilization, bank account, happiness

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,27 @@
-# your code goes here
+require "pry"
+
+class Person
+
+    attr_reader :name, :bank_account, :happiness
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+    end
+
+    def bank_account=(amount)
+        @bank_account = @bank_account + amount 
+    end
+
+    def happiness=(index)
+        # binding.pry
+        if index > 10 
+            @happiness = 10
+        elsif index < 0
+            @happiness = 0
+        else
+            @happiness = index
+        end
+    end
+end
